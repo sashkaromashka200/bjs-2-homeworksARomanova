c69e4d6bc3f3e2264566ca4b4a1c44b0fcf05f4d
Aleksandra Romanova <sashkaromashka200@gmail.com>
2025-06-21 22:31:03 +0300

    change base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    if (a === 0) {
+        throw new Error("Коэффициент a не может быть равен нулю");
+    }
+    
+    let arr = [];
+    
+    const discriminant = b ** 2 - 4 * a * c;
+    
+    if (discriminant < 0) {
+        return arr;
+    } else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        arr.push(root);
+    } else {
+        const sqrtD = Math.sqrt(discriminant);
+        const root1 = (-b + sqrtD) / (2 * a);
+        const root2 = (-b - sqrtD) / (2 * a);
+        arr.push(root1, root2);
+    }
+    
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    // Эта функция осталась без изменений, так как она не относится к заданию
 }
