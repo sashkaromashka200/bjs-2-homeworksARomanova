63094a243c8716d880f4a5a61bfe8f99ddbd59be
Aleksandra Romanova <sashkaromashka200@gmail.com>
2025-09-19 19:13:28 +0300

    работа выполнена

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -26,18 +26,20 @@ class Triangle {
       throw new Error("Треугольник с такими сторонами не существует");
     }
   }
-  
-    isValidTriangle() {
+
+  isValidTriangle() {
     return this.a + this.b > this.c && 
            this.a + this.c > this.b && 
-           this.b + this.c > this.a;
+           this.b + this.c > this.a &&
+           this.a > 0 && this.b > 0 && this.c > 0;
   }
   
+  // Геттер для периметра
   get perimeter() {
     return this.a + this.b + this.c;
   }
   
-  get area() {
+   get area() {
     const p = this.perimeter / 2;
     const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
     return parseFloat(area.toFixed(3));
@@ -48,7 +50,7 @@ function getTriangle(a, b, c) {
   try {
     return new Triangle(a, b, c);
   } catch (error) {
-    
+
     return {
       get area() {
         return "Ошибка! Треугольник не существует";
