6b360a585ba38ed4ecf8db182fbab0039a5c5f0d
Aleksandra Romanova <sashkaromashka200@gmail.com>
2025-09-19 20:17:03 +0300

    работа выполнена

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,63 +1,47 @@
 ﻿function parseCount(value) {
-    const parsed = Number.parseInt(value, 10);
-    if (isNaN(parsed)) {
+    const result = Number.parseInt(value);
+    if (isNaN(result)) {
         throw new Error("Невалидное значение");
     }
-    return parsed;
+    return result;
 }
 
 function validateCount(value) {
     try {
         return parseCount(value);
-    } catch (error) {
-        return error;
+    } catch (e) {
+        return e;
     }
 }
 
 class Triangle {
     constructor(a, b, c) {
-        if (a + b <= c || a + c <= b || b + c <= a) {
+        if (a + b < c || a + c < b || b + c < a) {
             throw new Error("Треугольник с такими сторонами не существует");
         }
-        
         this.a = a;
         this.b = b;
         this.c = c;
     }
-
+    
     getPerimeter() {
         return this.a + this.b + this.c;
     }
-
+    
     getArea() {
         const p = this.getPerimeter() / 2;
         const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-        return Number(area.toFixed(3));
+        return +area.toFixed(3);
     }
 }
 
 function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
-    } catch (error) {
-        const errorObj = {
-            getPerimeter: function() {
-                return "Ошибка! Треугольник не существует";
-            },
-            getArea: function() {
-                return "Ошибка! Треугольник не существует";
-            }
+    } catch (e) {
+        return {
+            getPerimeter: () => "Ошибка! Треугольник не существует",
+            getArea: () => "Ошибка! Треугольник не существует"
         };
-        
-        Object.defineProperty(errorObj, 'getPerimeter', {
-            writable: false,
-            configurable: false
-        });
-        Object.defineProperty(errorObj, 'getArea', {
-            writable: false,
-            configurable: false
-        });
-        
-        return errorObj;
     }
 }
